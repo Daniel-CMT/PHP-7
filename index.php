6c8bd629049234c48968ada6c8fdb27d77d0f562
Daniel-CMT <61702198+Daniel-CMT@users.noreply.github.com>
2020-03-18 11:04:29 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Daniel" => [
+        "city" => "Voves",
+        "weapon" => "crowbar"
+    ]
+];
+
+var_dump($characters);
